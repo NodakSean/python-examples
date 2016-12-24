41026ec46858ef0e54d4cb3d14eb7d81ec5f4a53
Sean Johnson <sean.a.johnson.2@gmail.com>
2016-12-23 21:20:19 -0600

    Add __repr__()

--- example-class.py
+++ example-class.py
@@ -31,25 +31,31 @@ class Employee:
     def employee_count(self):
         return Employee._number_of_employees
 
-    @classmethod
-    def increment_employee_count(cls):
-        cls._number_of_employees += 1
-
     def __str__(self):
         return "Name:{} | Email: {} | ID: {}".format(self.fullname, self.email_address, self.employee_id)
 
+    def __repr__(self):
+        return "Employee({}, {})".format(self.first_name, self.last_name)
 
+print()
 a = Employee("John", "Smith")
 a.employee_id = 21
 print("Employee Count: {}".format(a.employee_count))
 print(a)
+print(repr(a))
+
+print()
 
 b = Employee("Mike", "Gordon")
 b.employee_id = 42
 print("Employee Count: {}".format(b.employee_count))
 print(b)
+print(repr(b))
+
+print()
 
 print("{} > {}: {}".format(a.employee_id, b.employee_id, a.compare(a, b)))
+
 
 
 
